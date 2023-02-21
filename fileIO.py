8ac8fd5cabe0e18f3788bbfc0e589121d4165606
Adam Piwoński <88802068+Madapiw@users.noreply.github.com>
2023-02-21 21:03:10 +0100

    fileio init

---
+++ fileIO.py
@@ -0,0 +1,36 @@
+import librosa as lb
+import soundfile as sf
+
+class fileIO:
+    def __init__(self, filepath, savepath: None) -> None:
+        self.filepath = filepath
+        self.fileextension = filepath.split(".")[-1:]
+        self.savepath = savepath
+        self.audioobj = None
+        self.samplerate = None
+        
+    def loadFile(self):
+        if lb.util.valid_audio(self.filepath):
+            self.audioobj, self.samplerate = sf.read(self.filepath)
+        else:
+            return Exception("Bad file")
+    
+    def saveFile(self, objtosave, savepath, savesamplerate: None):
+        fileext = savepath.split(".")[-1]
+        if savesamplerate != None:
+            return sf.write(savepath, objtosave, savesamplerate)
+        else:
+            return sf.write(savepath, objtosave, self.samplerate)
+        
+    def getAudioObj(self):
+        return self.audioobj
+    
+    def getFilePath(self):
+        return self.filepath
+    
+    def get
+
+    
+    
+    
+    
